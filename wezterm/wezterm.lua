ec2b471eab411f513c67107d2a10bd779adde8b8
VKVERMA1122 <67781846+VKVERMA1122@users.noreply.github.com>
2023-12-17 21:00:42 +0530

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,9 @@
 -- The only required line is this one.
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 return {
+  hyperlink_rules = wezterm.default_hyperlink_rules(),
   color_scheme = "Gruvbox dark, soft (base16)",
   font_size = 11,
   window_background_opacity = 0.95,
@@ -11,4 +13,38 @@ return {
   use_fancy_tab_bar = false,
   inactive_pane_hsb = { saturation = 1, brightness = 1 }, -- s0.9, b0.8
   window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+  window_close_confirmation = 'NeverPrompt',
+  leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
+  keys = {
+    --splits
+    {
+      key = '|',
+      mods = 'LEADER|SHIFT',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = '\\',
+      mods = 'LEADER',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+
+    --tabs
+    {
+      key = 's',
+      mods = 'LEADER|CTRL',
+      action = wezterm.action.ActivateLastTab,
+    },
+    {
+      key = 't',
+      mods = 'SHIFT|CTRL',
+      action = act.SpawnTab 'CurrentPaneDomain',
+    },
+
+    --cmd palette
+    {
+      key = 'P',
+      mods = 'CTRL',
+      action = wezterm.action.ActivateCommandPalette,
+    }
+  }
 }
