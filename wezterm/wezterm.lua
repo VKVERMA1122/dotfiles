65a4fc9869bae5d178e6d0e1b59db8996917e3b4
VKVERMA1122 <67781846+VKVERMA1122@users.noreply.github.com>
2025-02-13 17:15:59 +0530

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,56 +1,106 @@
-he only required line is this one.
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 --config
 return {
-  hyperlink_rules = wezterm.default_hyperlink_rules(),
-  --color_scheme = "Gruvbox dark, soft (base16)",
-  font_size = 11,
-  hide_tab_bar_if_only_one_tab = true,
-  use_fancy_tab_bar = false,
-  tab_bar_at_bottom = true,
-  font = wezterm.font('JetBrains Mono', { weight = 'DemiBold' }),
-  window_decorations = "NONE",
-  inactive_pane_hsb = { saturation = 1, brightness = 1 }, -- s0.9, b0.8
-  window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-  window_close_confirmation = 'NeverPrompt',
-  leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
-  keys = {
-    --splits
-    {
-      key = '|',
-      mods = 'LEADER|SHIFT',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = '\\',
-      mods = 'LEADER',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'w',
-      mods = 'CTRL',
-      action = wezterm.action.CloseCurrentPane { confirm = true },
-    },
+	-- hyperlink_rules = wezterm.default_hyperlink_rules(),
+	-- default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" },
+	default_prog = { "C:\\Program Files\\WindowsApps\\Microsoft.PowerShell_7.5.0.0_x64__8wekyb3d8bbwe\\pwsh.exe" },
+	color_scheme = "GruvboxDarkHard",
+	font_size = 10.2,
+	hide_tab_bar_if_only_one_tab = true,
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = false,
+	font = wezterm.font("FiraCode NFM", { weight = "Medium" }),
+	-- window_background_opacity = 0.95,
+	window_decorations = "RESIZE",
+	inactive_pane_hsb = { saturation = 1, brightness = 1 }, -- s0.9, b0.8
+	window_padding = { left = 0, right = 0, top = 0, bottom = "0.0cell" },
+	window_close_confirmation = "NeverPrompt",
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = {
+		--splits
+		{
+			key = "|",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "\\",
+			mods = "LEADER",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "x",
+			mods = "CTRL",
+			action = wezterm.action.CloseCurrentPane({ confirm = false }),
+		},
 
-    --tabs
-    {
-      key = 's',
-      mods = 'LEADER|CTRL',
-      action = wezterm.action.ActivateLastTab,
-    },
-    {
-      key = 't',
-      mods = 'SHIFT|CTRL',
-      action = act.SpawnTab 'CurrentPaneDomain',
-    },
+		--tabs
+		{
+			key = "s",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.ActivateLastTab,
+		},
+		{
+			key = "t",
+			mods = "SHIFT|CTRL",
+			action = act.SpawnTab("CurrentPaneDomain"),
+		},
 
-    --cmd palette
-    {
-      key = 'P',
-      mods = 'CTRL',
-      action = wezterm.action.ActivateCommandPalette,
-    },
-  }
+		--cmd palette
+		{
+			key = "P",
+			mods = "CTRL",
+			action = wezterm.action.ActivateCommandPalette,
+		},
+
+		--copy
+		{
+			key = "LeftArrow",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.DisableDefaultAssignment,
+		},
+		{
+			key = "RightArrow",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.DisableDefaultAssignment,
+		},
+	},
+	hyperlink_rules = {
+		-- Matches: a URL in parens: (URL)
+		{
+			regex = "\\((\\w+://\\S+)\\)",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in brackets: [URL]
+		{
+			regex = "\\[(\\w+://\\S+)\\]",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in curly braces: {URL}
+		{
+			regex = "\\{(\\w+://\\S+)\\}",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in angle brackets: <URL>
+		{
+			regex = "<(\\w+://\\S+)>",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Then handle URLs not wrapped in brackets
+		{
+			-- Before
+			--regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+			--format = '$0',
+			-- After
+			regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+			format = "$1",
+			highlight = 1,
+		},
+	},
 }
