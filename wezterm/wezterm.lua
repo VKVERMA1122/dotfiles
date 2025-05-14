b01c53c480e4eedd4537be4d95a1528f67b8cd38
VKVERMA1122 <67781846+VKVERMA1122@users.noreply.github.com>
2025-05-14 22:16:30 +0530

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,28 +1,35 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
---config
 return {
 	-- hyperlink_rules = wezterm.default_hyperlink_rules(),
-	-- default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" },
-	front_end = "OpenGL",
-	max_fps = 144,
-	default_cursor_style = "BlinkingBlock",
 	animation_fps = 1,
-	cursor_blink_rate = 500,
-	term = "xterm-256color", -- Set the terminal type
 	prefer_egl = true,
-	window_background_image = "./wallhaven-85yl61_1920x1080.png",
+	-- window_background_gradient = {
+	-- 	orientation = "Vertical",
+	-- 	colors = {
+	-- 		"#1a1b26", -- Darker shade (top)
+	-- 		"#1f2335", -- Lighter shade (bottom)
+	-- 	},
+	-- },
+	-- window_background_image = 'C:\\Users\\LENOVO\\.config\\wezterm\\wallhaven-85yl61_1920x1080.png',
+	-- Set the overall window opacity to reduce the prominence of the background image
+	-- Adjust the brightness, saturation, and hue of the background image
+	-- window_background_image_hsb = {
+	-- 	brightness = 0.1, -- Adjust the brightness (0.0 is fully dark, 1.0 is fully bright)
+	-- 	saturation = 0.1, -- Adjust the saturation (0.0 is fully desaturated, 1.0 is fully saturated)
+	-- 	hue = 0.0,  -- Adjust the hue (0.0 to 1.0)
+	-- },
 	default_prog = { "C:\\Program Files\\WindowsApps\\Microsoft.PowerShell_7.5.0.0_x64__8wekyb3d8bbwe\\pwsh.exe" },
 	color_scheme = "tokyonight_night",
-	font_size = 10.2,
+	font_size = 10,
 	hide_tab_bar_if_only_one_tab = true,
 	use_fancy_tab_bar = false,
 	tab_bar_at_bottom = false,
 	font = wezterm.font("FiraCode NFM", { weight = "Medium" }),
 	window_background_opacity = 0.94,
 	window_decorations = "RESIZE",
-	inactive_pane_hsb = { saturation = 1, brightness = 1 }, -- s0.9, b0.8
+	inactive_pane_hsb = { saturation = 1, brightness = 1 },
 	window_padding = { left = 0, right = 0, top = 0, bottom = "0.0cell" },
 	window_close_confirmation = "NeverPrompt",
 	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
